d72f439c8c599c297313dc2c3c0f2739a0d8d553
Chantelle Bassey <cxb1127@student.bham.ac.uk>
2025-10-31 19:15:24 +0000

    Implement dielectric scattering logic

--- RayTracing/src/material.h
+++ RayTracing/src/material.h
@@ -71,6 +71,37 @@ public:
 
 	bool scatter(ray& current_ray, ray& scattered_ray, const hit_info& hit_info, glm::vec3& attenuation) const override
 	{
+		glm::vec3 normal = hit_info.world_normal;
+		float n1 = 1.0f;
+		float n2 = refractive_index;
+		float cos_theta = fmin(dot(current_ray.direction, normal), 1.0f);
+
+		if (cos_theta > 0.0f)
+		{
+			normal = -normal;
+			std::swap(n1, n2);
+			cos_theta = -cos_theta;
+		}
+
+		float sin_theta = std::sqrt(1 - cos_theta * cos_theta);
+		float eta = n1 / n2;
+
+		if (eta * sin_theta > 1.0f)
+		{
+			glm::vec3 reflection = reflect(current_ray.direction, normal);
+			scattered_ray.direction = normalize(reflection);
+		}
+		else
+		{
+			cos_theta = fmin(dot(-current_ray.direction, normal), 1.0f);
+			glm::vec3 refracted_perp = eta * (current_ray.direction + cos_theta * normal);
+			glm::vec3 refracted_parallel = -std::sqrt(std::fabs(1.0f - glm::dot(refracted_perp, refracted_perp))) * normal;
+			scattered_ray.direction = refracted_perp + refracted_parallel;
+		}
+
+		float offset_dir = (glm::dot(scattered_ray.direction, normal) > 0.0f) ? 1.0f : -1.0f;
+		scattered_ray.origin = hit_info.world_position + 0.001f * offset_dir * normal;
+		attenuation = albedo;
 		return true;
 	}
 };
