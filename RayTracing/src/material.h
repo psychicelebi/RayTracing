226d9b2792dffbe5a17112ea8b70f118812bd24d
Chantelle Bassey <cxb1127@student.bham.ac.uk>
2025-11-01 19:36:49 +0000

    Refactor and enhance dielectric material handling

--- RayTracing/src/material.h
+++ RayTracing/src/material.h
@@ -3,6 +3,7 @@
 #include <Walnut/Random.h>
 #include "ray.h"
 #include "hit_info.h"
+#include <random>
 
 
 class material
@@ -28,7 +29,8 @@ public:
 	}
 
 private:
-	glm::vec3 diffuse_reflect(const glm::vec3& normal) const {
+	glm::vec3 diffuse_reflect(const glm::vec3& normal) const 
+	{
 		glm::vec3 vector{ 0.0f };
 		do
 		{
@@ -67,34 +69,40 @@ public:
 class dielectric : public material
 {
 public:
-	float refractive_index = 1.0f;
+	float refractive_index = 1.5f;
 
 	bool scatter(ray& current_ray, ray& scattered_ray, const hit_info& hit_info, glm::vec3& attenuation) const override
 	{
+		static std::mt19937 rng{ std::random_device{}() };
+		static std::uniform_real_distribution<float> probability(0.0f, 1.0f);
+
 		glm::vec3 normal = hit_info.world_normal;
 		float n1 = 1.0f;
 		float n2 = refractive_index;
-		float cos_theta = fmin(dot(current_ray.direction, normal), 1.0f);
+		float cos_i = fmin(dot(current_ray.direction, normal), 1.0f); // cosine of incident angle
 
-		if (cos_theta > 0.0f)
+		if (cos_i > 0.0f)
 		{
 			normal = -normal;
 			std::swap(n1, n2);
-			cos_theta = -cos_theta;
+			cos_i = -cos_i;
 		}
 
-		float sin_theta = std::sqrt(1 - cos_theta * cos_theta);
+		float sin_i = std::sqrt(1 - cos_i * cos_i); // sine of incident angle
 		float eta = n1 / n2;
+		float reflectance = calculate_reflectance(std::fabs(cos_i), n1, n2);
 
-		if (eta * sin_theta > 1.0f)
+		// std::cout << "reflectance: " << reflectance << std::endl;
+
+		if (eta * sin_i > 1.0f || reflectance >= probability(rng))
 		{
 			glm::vec3 reflection = reflect(current_ray.direction, normal);
 			scattered_ray.direction = normalize(reflection);
 		}
 		else
 		{
-			cos_theta = fmin(dot(-current_ray.direction, normal), 1.0f);
-			glm::vec3 refracted_perp = eta * (current_ray.direction + cos_theta * normal);
+			cos_i = fmin(dot(-current_ray.direction, normal), 1.0f);
+			glm::vec3 refracted_perp = eta * (current_ray.direction + cos_i * normal);
 			glm::vec3 refracted_parallel = -std::sqrt(std::fabs(1.0f - glm::dot(refracted_perp, refracted_perp))) * normal;
 			scattered_ray.direction = refracted_perp + refracted_parallel;
 		}
@@ -103,5 +111,13 @@ public:
 		scattered_ray.origin = hit_info.world_position + 0.001f * offset_dir * normal;
 		attenuation = albedo;
 		return true;
+	}
+
+private:
+	double calculate_reflectance(const float& cos_i, const float& n1, const float& n2) const
+	{
+		float base_reflectance = (n1 - n2) / (n1 + n2);
+		base_reflectance *= base_reflectance;
+		return base_reflectance + (1.0f - base_reflectance) * pow((1.0f - cos_i), 5);
 	}
 };
